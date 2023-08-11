220554684e5701e53cf002c5b82a092fc8ceb637
Melonie de Almeida <melonien96e@gmail.com>
2023-08-11 00:57:38 +0000

    retrained

--- future_frame_generation/ffp_lora_double_cond.sh
+++ future_frame_generation/ffp_lora_double_cond.sh
@@ -1,7 +1,7 @@
 export MODEL_NAME="runwayml/stable-diffusion-inpainting"
-export DATA_PATH="data_prep/test_small/target_frames"
-export COND_PATH="data_prep/test_small/previous_frames"
-export COND_PATH2="data_prep/test_small/processed_frames"
+export DATA_PATH="data_prep/one_sample/target_frames"
+export COND_PATH="data_prep/one_sample/previous_frames"
+export COND_PATH2="data_prep/one_sample/processed_frames"
 export TEXT_TARGET="tennis"
 
 accelerate launch --mixed_precision="fp16"  ffp.py \
@@ -12,9 +12,9 @@ accelerate launch --mixed_precision="fp16"  ffp.py \
   --cond_data_dir2=$COND_PATH2 \
   --instance_prompt=$TEXT_TARGET \
   --class_prompt=$TEXT_TARGET \
-  --train_batch_size=3 \
+  --train_batch_size=1 \
   --sample_batch_size=1 \
-  --output_dir="outsample7" \
+  --output_dir="outsample1" \
   --gradient_accumulation_steps=1 \
   --max_train_steps=25000\
   --learning_rate=1e-04 \
