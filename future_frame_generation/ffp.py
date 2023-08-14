ec11f2cadc690fb1eb7e4b3dadb3554e5ccfd01e
Melonie de Almeida <melonien96e@gmail.com>
2023-08-14 15:03:57 +0000

    threshold_tuning

--- future_frame_generation/ffp.py
+++ future_frame_generation/ffp.py
@@ -33,48 +33,6 @@ check_min_version("0.13.0.dev0")
 logger = get_logger(__name__)
 
 
-def prepare_mask_and_masked_image(image, mask):
-    image = np.array(image.convert("RGB"))
-    image = image[None].transpose(0, 3, 1, 2)
-    image = torch.from_numpy(image).to(dtype=torch.float32) / 127.5 - 1.0
-
-    mask = np.array(mask.convert("L"))
-    mask = mask.astype(np.float32) / 255.0
-    mask = mask[None, None]
-    mask[mask < 0.5] = 0
-    mask[mask >= 0.5] = 1
-    mask = torch.from_numpy(mask)
-
-    masked_image = image * (mask < 0.5)
-
-    return mask, masked_image
-
-
-# generate random masks
-def random_mask(im_shape, ratio=1, mask_full_image=False):
-    mask = Image.new("L", im_shape, 0)
-    draw = ImageDraw.Draw(mask)
-    size = (random.randint(0, int(im_shape[0] * ratio)), random.randint(0, int(im_shape[1] * ratio)))
-    # use this to always mask the whole image
-    if mask_full_image:
-        size = (int(im_shape[0] * ratio), int(im_shape[1] * ratio))
-    limits = (im_shape[0] - size[0] // 2, im_shape[1] - size[1] // 2)
-    center = (random.randint(size[0] // 2, limits[0]), random.randint(size[1] // 2, limits[1]))
-    draw_type = random.randint(0, 1)
-    if draw_type == 0 or mask_full_image:
-        draw.rectangle(
-            (center[0] - size[0] // 2, center[1] - size[1] // 2, center[0] + size[0] // 2, center[1] + size[1] // 2),
-            fill=255,
-        )
-    else:
-        draw.ellipse(
-            (center[0] - size[0] // 2, center[1] - size[1] // 2, center[0] + size[0] // 2, center[1] + size[1] // 2),
-            fill=255,
-        )
-
-    return mask
-
-
 def parse_args():
     parser = argparse.ArgumentParser(description="Simple example of a training script.")
     parser.add_argument(
@@ -102,7 +60,7 @@ def parse_args():
         type=str,
         default=None,
         required=True,
-        help="A folder containing the training data of instance images.",
+        help="A folder containing the training data of previous frames.",
     )
 
     parser.add_argument(
@@ -110,7 +68,15 @@ def parse_args():
         type=str,
         default=None,
         required=True,
-        help="A folder containing the training data of instance images.",
+        help="A folder containing the training data of previous of previous frames.",
+    )
+
+    parser.add_argument(
+        "--threshold",
+        type=int,
+        default=None,
+        required=True,
+        help="Threshold when getting pixel-wise difference.",
     )
 
     parser.add_argument(
@@ -314,10 +280,32 @@ def parse_args():
 
     return args
 
-# def binarize_tensor(tensor, threshold):
-#     binary_tensor = tensor > threshold
-#     binary_tensor = binary_tensor.float()
-#     return binary_tensor
+def pixel_wise_difference(image1, image2, threshold):
+
+    # Ensure the images have the same size
+    if image1.size != image2.size:
+        raise ValueError("Images must have the same size.")
+
+    # Get the pixel data for both images
+    pixels1 = image1.load()
+    pixels2 = image2.load()
+
+    # Create a new image to store the pixel-wise difference
+    diff_image = Image.new('RGB', image1.size)
+    pixels_diff = diff_image.load()
+
+    # Calculate the pixel-wise difference
+    for x in range(image1.width):
+        for y in range(image1.height):
+            r_diff = abs(pixels1[x, y][0] - pixels2[x, y][0])
+            g_diff = abs(pixels1[x, y][1] - pixels2[x, y][1])
+            b_diff = abs(pixels1[x, y][2] - pixels2[x, y][2])
+            pixels_diff[x, y] = (r_diff, g_diff, b_diff)
+
+    diff_image_gray = diff_image.convert('L')
+    diff_image_bin = diff_image_gray.point(lambda p: 0 if p < threshold else 255)
+
+    return diff_image_bin
 
 class DreamBoothDataset(Dataset):
     """
@@ -332,6 +320,7 @@ class DreamBoothDataset(Dataset):
         cond_data_root2,
         instance_prompt,
         tokenizer,
+        threshold,
         class_data_root=None,
         class_prompt=None,
         size=512,
@@ -340,6 +329,7 @@ class DreamBoothDataset(Dataset):
         self.size = size
         self.center_crop = center_crop
         self.tokenizer = tokenizer
+        self.threshold = threshold
 
         self.instance_data_root = Path(instance_data_root)
         if not self.instance_data_root.exists():
@@ -351,7 +341,7 @@ class DreamBoothDataset(Dataset):
 
         self.cond_data_root2 = Path(cond_data_root2)
         if not self.cond_data_root2.exists():
-            raise ValueError("Previous frames root doesn't exists.")
+            raise ValueError("Previous of previous frames root doesn't exists.")
 
         self.instance_images_path = list(Path(instance_data_root).iterdir())
         self.cond_images_path = list(Path(cond_data_root).iterdir())
@@ -404,17 +394,21 @@ class DreamBoothDataset(Dataset):
             instance_image = instance_image.convert("RGB")
         if not cond_image.mode == "RGB":
             cond_image = cond_image.convert("RGB")
-        if not cond_image2.mode == "L":
-            cond_image2 = cond_image2.convert("L")
+        if not cond_image2.mode == "RGB":
+            cond_image2 = cond_image2.convert("RGB")
 
         instance_image = self.image_transforms_resize_and_crop(instance_image)
         cond_image = self.image_transforms_resize_and_crop(cond_image)
-        cond_image2 = self.image_transforms_resize_and_crop(cond_image2)
+        temp_image = self.image_transforms_resize_and_crop(cond_image2)
+        cond_image2 = pixel_wise_difference(cond_image,temp_image,self.threshold)
+        cond_image2.save("temp.jpg")
+
 
         example["PIL_images"] = instance_image
         example["instance_images"] = self.image_transforms_normalize(instance_image)
         example["cond_images2"] =  self.image_transforms(cond_image2).reshape([1,1,self.size,self.size])
         example["cond_images"] =  self.image_transforms_normalize(cond_image)
+
 
         example["instance_prompt_ids"] = self.tokenizer(
             self.instance_prompt,
@@ -484,52 +478,6 @@ def main():
     if args.seed is not None:
         set_seed(args.seed)
 
-    if args.with_prior_preservation:
-        class_images_dir = Path(args.class_data_dir)
-        if not class_images_dir.exists():
-            class_images_dir.mkdir(parents=True)
-        cur_class_images = len(list(class_images_dir.iterdir()))
-
-        if cur_class_images < args.num_class_images:
-            torch_dtype = torch.float16 if accelerator.device.type == "cuda" else torch.float32
-            pipeline = StableDiffusionInpaintPipeline.from_pretrained(
-                args.pretrained_model_name_or_path, torch_dtype=torch_dtype, safety_checker=None
-            )
-            pipeline.set_progress_bar_config(disable=True)
-
-            num_new_images = args.num_class_images - cur_class_images
-            logger.info(f"Number of class images to sample: {num_new_images}.")
-
-            sample_dataset = PromptDataset(args.class_prompt, num_new_images)
-            sample_dataloader = torch.utils.data.DataLoader(
-                sample_dataset, batch_size=args.sample_batch_size, num_workers=1
-            )
-
-            sample_dataloader = accelerator.prepare(sample_dataloader)
-            pipeline.to(accelerator.device)
-            transform_to_pil = transforms.ToPILImage()
-            for example in tqdm(
-                sample_dataloader, desc="Generating class images", disable=not accelerator.is_local_main_process
-            ):
-                bsz = len(example["prompt"])
-                fake_images = torch.rand((3, args.resolution, args.resolution))
-                transform_to_pil = transforms.ToPILImage()
-                fake_pil_images = transform_to_pil(fake_images)
-
-                fake_mask = random_mask((args.resolution, args.resolution), ratio=1, mask_full_image=True)
-
-                images = pipeline(prompt=example["prompt"], mask_image=fake_mask, image=fake_pil_images).images
-
-                for i, image in enumerate(images):
-                    hash_image = hashlib.sha1(image.tobytes()).hexdigest()
-                    image_filename = class_images_dir / f"{example['index'][i] + cur_class_images}-{hash_image}.jpg"
-                    image.save(image_filename)
-
-            del pipeline
-            if torch.cuda.is_available():
-                torch.cuda.empty_cache()
-
-    # Handle the repository creation
     if accelerator.is_main_process:
         if args.output_dir is not None:
             os.makedirs(args.output_dir, exist_ok=True)
@@ -645,6 +593,7 @@ def main():
         tokenizer=tokenizer,
         size=args.resolution,
         center_crop=args.center_crop,
+        threshold=args.threshold
     )
 
     def collate_fn(examples):
@@ -661,28 +610,6 @@ def main():
             pixel_values += [example["class_images"] for example in examples]
             pior_pil = [example["class_PIL_images"] for example in examples]
 
-        # masks = []
-        # masked_images = []
-        # for example in examples:
-        #     pil_image = example["PIL_images"]
-            # generate a random mask
-            # mask = random_mask(pil_image.size, 1, False)
-            # prepare mask and masked image
-            # mask, masked_image = prepare_mask_and_masked_image(pil_image, mask)
-
-            # masks.append(mask)
-            # masked_images.append(masked_image)
-
-        # if args.with_prior_preservation:
-        #     for pil_image in pior_pil:
-        #         # generate a random mask
-        #         mask = random_mask(pil_image.size, 1, False)
-        #         # prepare mask and masked image
-        #         mask, masked_image = prepare_mask_and_masked_image(pil_image, mask)
-
-        #         masks.append(mask)
-        #         masked_images.append(masked_image)
-
         pixel_values = torch.stack(pixel_values)
         pixel_values = pixel_values.to(memory_format=torch.contiguous_format).float()
 
@@ -693,8 +620,6 @@ def main():
         cond_pixel_values2 = cond_pixel_values2.to(memory_format=torch.contiguous_format).float()
 
         input_ids = tokenizer.pad({"input_ids": input_ids}, padding=True, return_tensors="pt").input_ids
-        # masks = torch.stack(masks)
-        # masked_images = torch.stack(masked_images)
         batch = {"input_ids": input_ids, "pixel_values": pixel_values, "masks": cond_pixel_values2, "masked_images": cond_pixel_values}
         return batch
 
